97e1ea488608c8ec9c8b4a9798017a3f46d1cb90
athebs <athena.s.tafur@gmail.com>
2024-01-31 19:12:52 -0600

    not sure where the last update was here is where I am at now

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,41 @@
 function fibonacci(num) {
-  return 0;
-}
+
+  /** f(n) =  F(n-1) + f(n-2)
+   * f= num and n is the index of num 
+   * intial values are 0 1 
+   * 
+   * set arr [0,1]
+   * sum values of arr 
+   * append value to arr 
+   * for loop in range of num 
+   */
+    if (num === 0 || num === 1 ){
+      return num
+    } else {
+      return sequenceFinder(num)
+    }
+  }
+  
+  function sequenceFinder(num){
+    let numOne = 0;
+    let numTwo = 1;
+    let numThree;
+    
+    for ( let i = 2; i<= num; i++){
+    
+      numThree = numOne + numTwo
+      
+      numOne = numTwo;
+      numTwo = numThree;
+    
+    }
+    return numThree;
+  }
+    
+  }
+
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+fibonacci(5)
 
 module.exports = fibonacci;
